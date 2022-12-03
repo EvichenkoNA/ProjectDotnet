5012894d84bbb6b47e5b8bf4825c33c0a9a7a7e0
Natalia <natalia.evichenko@mail.ru>
2022-12-03 16:11:37 +0300

    Task added. Finding the maximum of three numbers.

---
+++ Homework1_Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите три числа: ");    //Просим пользователя ввести данные
+
+string? inputLine1 = Console.ReadLine();     //Считываем данные с консоли
+string? inputLine2 = Console.ReadLine();
+string? inputLine3 = Console.ReadLine();
+
+if(inputLine1!=null&&inputLine2!=null&&inputLine3!=null)       //Проверяем, чтобы данные были не пустыми
+{
+    int number1 = int.Parse(inputLine1);       //Парсим введенные числа
+    int number2 = int.Parse(inputLine2);
+    int number3 = int.Parse(inputLine3);
+    
+    if(number1>number2 && number1>number3)
+    {
+        Console.WriteLine("max = "+ number1);
+    }
+    else
+    {
+        if(number2>number1 && number2>number3)
+        {
+            Console.WriteLine("max = "+ number2);
+        }
+        else
+        {
+            Console.WriteLine("max = "+ number3);
+        }
+    }
+}
