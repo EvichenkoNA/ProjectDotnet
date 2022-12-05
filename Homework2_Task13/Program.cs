fe6fe500277603df27dc03bd7b09cc7b922979cb
Natalia <natalia.evichenko@mail.ru>
2022-12-05 15:52:23 +0300

    Task added. Third digit of the number.

---
+++ Homework2_Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// Первый способ
+
+Console.WriteLine("Введите число: ");       //Просим пользователя ввести данные
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number >= 100)                     //Делаем проверку, действительно ли число явяетя трехзначным
+{
+    char[] numberArray = number.ToString().ToCharArray();  
+    Console.WriteLine(numberArray[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры в введенном числе нет");
+} 
+
+
