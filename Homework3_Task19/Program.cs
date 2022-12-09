7d6938c9d1952a6383e5fc20a8c29d13099db0d2
Natalia <natalia.evichenko@mail.ru>
2022-12-09 20:21:23 +0300

    Task added. Polindrom Test - hard.

--- Homework3_Task19/Program.cs
+++ Homework3_Task19/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// Метод читает данные от пользователя
+/* // Метод читает данные от пользователя
 int ReadData(string msg)
 {
     Console.WriteLine(msg);
@@ -28,8 +28,69 @@ if (number > 9999 && number < 100000)
     bool result = PolindromTest(number);
     PrintData("Является ли введенное число полиндромом? - ", PolindromTest(number));
 }
-else Console.WriteLine("Вы ввели не 5-ти значное число");
+else Console.WriteLine("Вы ввели не 5-ти значное число"); */
 
+
+// read output data
+int outputNumber = ReadData("Enter a five-digit number ");
+// cheking the correctness of the entered datd
+if (outputNumber>=10000&&outputNumber<=99999)    
+{
+    // create a dictionary with four-digit palondromes
+    var palindrome = new Dictionary<int, int>();
+    palindrome = DictionaryFill();
+    // check if there is an input number in dictionary  
+    bool check = PalinTest(outputNumber, palindrome);
+    if (check)
+    {
+        PrintData(outputNumber, " is a palindrome");
+    }
+    else
+    {
+        PrintData(outputNumber, " is not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("Input number isn't correct. It shoul be from 10000 to 99999");
+}
+
+// method read input data from console
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+// method print result to console 
+void PrintData(int num, string msg)
+{
+    Console.WriteLine(num+msg);
+}
+// method fill a dictionary with four-digit palindrome
+Dictionary<int, int> DictionaryFill()
+{ 
+    var dictionary = new Dictionary<int, int>();
+    int keyNumber = 1;
+        for(int secondNumber = 0; secondNumber<=9; secondNumber++)
+        {
+            for(int firstNumber = 1; firstNumber<=9; firstNumber++)
+            {
+                dictionary[keyNumber] = firstNumber*1000+secondNumber*100+secondNumber*10+firstNumber;
+                keyNumber++;
+            }
+        }
+    return dictionary;
+}
+// method check if the five-digit number is a palindrome
+bool PalinTest(int number, Dictionary<int, int> palindrome)
+{
+    bool result = false;
+    if (palindrome.ContainsValue((number/1000)*100+(number%100)))
+    {
+        result = true;
+    }
+    return result;
+}
 
 
 
