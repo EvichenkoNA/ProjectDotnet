f8b5fb49a3d169153c0bb1592aabb56f307311b0
Natalia <natalia.evichenko@mail.ru>
2022-12-13 00:41:40 +0300

    Task added. Random choice of name.

---
+++ Homework4_Task+/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая способна случайным образом выбрать человека из введенного списка
+
+string ReadStringData(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+string NameSelect(string str)
+{
+    string[] names = str.Split(", ");
+    string name = names[new Random().Next(0, names.Length)];
+    return name;
+}
+
+string str = ReadStringData("Введите список имен (с пробелом через запятую): ");
+PrintData("" + NameSelect(str));
