c0594ef95bdcd5c6e0b844fbebba551acbe79bfa
Natalia <natalia.evichenko@mail.ru>
2022-12-18 14:13:09 +0300

    Task added. Converting numbers to different systems.

---
+++ Seminar6_Task42/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int inputNumber = ReadData("Enter number: ");
+
+PrintData("Result in bynary system: " + DegToBin(inputNumber));
+PrintData("Result in bynary system: " + DecToBinNativ(inputNumber));
+PrintData("Result in octal system: " + Convert.ToString(inputNumber, 8));           // используются встроенные методы
+PrintData("Result in hexadecimal system: " + Convert.ToString(inputNumber, 16));
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+string DegToBin(int num)                // Метод, преобразующий десятичное число в двоичное
+{
+    string outBin = String.Empty;
+    while (num > 0)
+    {
+        outBin = (num % 2) + outBin;
+        num = num / 2;
+    }
+    return outBin;
+}
+
+string DecToBinNativ(int num)           // Встроенный метод, преобразующий десятичное число в двоичное
+{
+    return Convert.ToString(num, 2);
+}
